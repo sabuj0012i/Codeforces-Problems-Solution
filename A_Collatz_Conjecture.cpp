a1383e3c5a47836dae0f621f0abb0ed98ec0641b
sabuj0012i <sabujhossainraj2000@gmail.com>
2025-11-20 14:02:19 +0600

    Adding A Collatz Conjecture

---
+++ A_Collatz_Conjecture.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void solve(){
+    int k,x;cin>>k>>x;
+    while(k--){
+        x*=2;
+    }
+
+    cout<<x<<endl;
+
+}
+int main(){
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
