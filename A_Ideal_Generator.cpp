7776c2f74f667c9e1a84575b66ec45171b0dfb99
Sabuj <sabujhossainraj2000@gmail.com>
2026-01-07 22:45:12 +0600

    Ideal Generator problem solution

---
+++ A_Ideal_Generator.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int k;cin>>k;
+        if(k%2 == 0) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+
+    return 0;
+}
